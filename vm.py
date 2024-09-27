c98dfd8826fee1aa68798253f0c8f8f1872a3f97
NanodAaa <enderddr2@outlook.com>
2024-09-27 15:38:05 +0800

    update 1.0

--- vm.py
+++ vm.py
@@ -9,10 +9,13 @@ class VideoManager:
     INSTALL_PATH = ''
     FFMPEG_PATH = ''
     INPUT_PATH = ''
+    VIDEO_LIST = []
     
-    def __init__(self):
+    def __init__(self, input_path):
         self.FFMPEG_PATH = self.get_ffmpeg_path()
         self.INSTALL_PATH = os.path.dirname(os.path.abspath(__file__))
+        self.INPUT_PATH = input_path
+        self.VIDEO_LIST = self.get_video_list()
     
     def get_ffmpeg_path(self):
         """
@@ -32,19 +35,55 @@ class VideoManager:
         else: # linux
             return os.path.join(base_path, 'ffmpeg', 'bin' , 'ffmpeg')
         
-    def get_video_list(self):
+    def get_video_list(self, input_path=None):
         """ 
         Find all the video files in the input folder and display them on console.
+        
+        Args:
+            `input_path`: The path of the video or folder which contain the videos. Default = None.
+        
+        Return:
+            `video_list`: List include video name, video abspath and other video info.
         """
-        fileslist = os.listdir(self.INPUT_PATH)
+        
         video_list = []
+        
+        if input_path == None:
+            input_path = self.INPUT_PATH
+            
+        if os.path.isfile(input_path):
+            video_list = [
+                {
+                    'basename' : os.path.basename(input_path),
+                    'dirname' : os.path.split(input_path)[0],
+                    'format' : os.path.splitext(os.path.basename(input_path))[1],
+                    'abspath' : input_path
+                }
+            ]
+            return video_list
+        
+        fileslist = os.listdir(input_path)
         for file in fileslist:
-            if (file.endswith(".mp4") or file.endswith(".mkv") or file.endswith(".avi") or file.endswith(".flv") or file.endswith(".wmv")) or (file.endswith(".mov")) and (not file.startswith("output.mp4")):
-                video_list.append(file)
+            if (file.endswith(('mp4', 'mkv', 'avi', 'flv', 'mov'))) and (not file.startswith("output.mp4")):
+                basename = file
+                dirname = input_path
+                format = os.path.splitext(basename)[1]
+                abspath = os.path.join(input_path, file)
+                
+                video_list.append(
+                    {
+                        'basename' : basename,
+                        'dirname' : dirname,
+                        'format' : format,
+                        'abspath' : abspath
+                    }
+                )
                 
         print("# Video list: ")    
         for video in video_list:
-            print(video)
+            print(f"Filename: {video['basename']}, File abspath: {video['abspath']}")
+            
+        return video_list
         
     def get_video_info(self):
         """
@@ -77,9 +116,8 @@ class VideoMerger(VideoManager):
     FILELIST_TXT_PATH = ''
     
     def __init__(self, input_path):
-        super().__init__()
+        super().__init__(input_path)
         self.FILELIST_TXT_PATH = os.path.join(self.INSTALL_PATH, "filelist.txt")
-        self.INPUT_PATH = input_path
         
     def get_filelist_txt(self):
         """
@@ -117,7 +155,6 @@ class VideoMerger(VideoManager):
             '-f', 'concat',
             '-safe', '0', 
             '-i', self.FILELIST_TXT_PATH,
-            '-fflags', '+genpts',  # 添加此行以强制生成时间戳
             '-c', 'copy',
             output_path
         ]
@@ -130,12 +167,10 @@ class VideoMerger(VideoManager):
 class ThumbGenerator(VideoManager):
     """
     """
-    INPUT_PATH = ''
     CURRENT_PATH = ''
     
     def __init__(self, input_path):
-        super().__init__()
-        self.INPUT_PATH = input_path
+        super().__init__(input_path)
         self.CURRENT_PATH = self.INPUT_PATH
     
     def generate_thumbnails(self):
@@ -146,41 +181,87 @@ class ThumbGenerator(VideoManager):
             `dir_path`: the path of the input folder.
         """
 
-        dirs = os.listdir(self.CURRENT_PATH)
-        for object in dirs:
-            object_path = os.path.join(self.CURRENT_PATH, object)
-            if os.path.isdir(object_path):
-                self.CURRENT_PATH = object_path
-                self.generate_thumbnails()
-            else:
-                if object.endswith('.mp4') or object.endswith('.mkv') or object.endswith('.avi') or object.endswith('.flv') or object.endswith('.wmv') or object.endswith('.mov'):
-                    # print(os.path.join(dir_path, object))
-                    # 调用 ffmpeg 生成缩略图
-                    # print("input_path: " + input_path)    DEBUG
-                    input_path = object_path
-                    output_path = os.path.join(self.INPUT_PATH, os.path.splitext(object)[0])
-                    if (os.path.exists(output_path) == False):
-                        os.mkdir(output_path)
-                        
-                    else:
-                        output_path = os.path.join(output_path, 'out%02d.jpg')
-                    
-                    command = [self.FFMPEG_PATH, 
+        for video in self.VIDEO_LIST:
+            output_path = os.path.join(video['dirname'], os.path.splitext(video['basename'])[0])
+            if (os.path.exists(output_path) == False):
+                os.mkdir(output_path)
+                
+            output_path = os.path.join(output_path, 'out%02d.jpg')
+            
+            command = [self.FFMPEG_PATH, 
                             '-ss', '3',
-                            '-i', input_path, 
+                            '-i', video['abspath'], 
                             '-vf', 'select=gt(scene\,0.4)',
                             '-frames:v', '20', 
                             '-fps_mode', 'vfr',
                             output_path]
-                    try:
-                        # Call subprocess to run FFMPEG command.
-                        subprocess.run(command, check=True)
-                    
-                    except subprocess.CalledProcessError as e:
-                        print(f'ffmpeg process error: {e.returncode}')
-                    
-                else:
-                    pass
-        
-            self.CURRENT_PATH = os.path.split(self.CURRENT_PATH)[0]
+            try:
+                # Call subprocess to run FFMPEG command.
+                subprocess.run(command, check=True)
             
+            except subprocess.CalledProcessError as e:
+                print(f'ffmpeg process error: {e.returncode}')
+                
+        return
+            
+class FormatTransformer(VideoManager):
+    """
+    """
+    OUTPUT_FORMAT = ''
+    VALID_OUTPUT_FORMAT_TUPLE = ('mp4', 'mkv', 'avi', 'mov', 'flv')
+    
+    def __init__(self, input_path, output_format):
+        super().__init__(input_path)
+        self.OUTPUT_FORMAT = output_format
+        
+    def format_transform(self, input_path=None, output_format=None):
+        """
+        Transform video format.
+        """
+        
+        if not input_path == None:
+            video_list = self.get_video_list()
+            
+        else:
+            input_path = self.INPUT_PATH
+            video_list = self.VIDEO_LIST
+            
+        if not os.path.exists(input_path):
+            print('File does not exist!')
+            return -1
+            
+        if output_format == None:
+            output_format = self.OUTPUT_FORMAT
+            
+        if not output_format in self.VALID_OUTPUT_FORMAT_TUPLE:
+            print('Output format is invalid!')
+            return -1
+        
+        recode_flag = False
+        if (input('Wether you want to recode the video? (y/n): ').lower == 'y'):
+            recode_flag = True
+            
+        else:
+            for video in video_list:
+                
+                command = [
+                    self.FFMPEG_PATH,
+                    '-i', video['abspath'],
+                ]
+                
+                if recode_flag == False:
+                    command.append('-c')
+                    command.append('copy')
+                    
+                output_path = os.path.join(video['dirname'], 
+                                                os.path.splitext(video['basename'])[0]) + f'.{output_format}'
+                command.append(output_path)
+                
+                try:
+                    subprocess.run(command, check=True)
+                    
+                except subprocess.CalledProcessError as e:
+                    print(f'{e.returncode}')
+                    return -1
+            
+        return
